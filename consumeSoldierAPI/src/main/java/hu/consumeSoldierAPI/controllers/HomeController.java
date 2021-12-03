5511e048e2b2db34cb301576b0cebf1b95a3d066
marcu2401 <71020331+marcu2401@users.noreply.github.com>
2021-12-03 07:57:26 +0100

    Add files via upload

--- consumeSoldierAPI/src/main/java/hu/consumeSoldierAPI/controllers/HomeController.java
+++ consumeSoldierAPI/src/main/java/hu/consumeSoldierAPI/controllers/HomeController.java
@@ -1,5 +1,6 @@
 package hu.consumeSoldierAPI.controllers;
 
+import hu.consumeSoldierAPI.domain.Soldier;
 import hu.consumeSoldierAPI.services.SoldierService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
@@ -56,5 +57,31 @@ public class HomeController {
         return "soldiers";
     }
 
+    @PatchMapping(value="/soldiers")
+    public String updateSoldier(@RequestParam("id") int id,
+                                @RequestParam("shotpeople") int shot,
+                                Model model){
+        int statusCode = service.updateSoldier(id, shot);
+        model.addAttribute("statuscode", statusCode);
+        model.addAttribute("soldiers", service.getSoldiers());
+        return "soldiers";
+    }
 
+    @GetMapping("/soldiers/replace/{id}")
+    public String replace(){
+        return "replace";
+    }
+
+    @PutMapping("/soldiers")
+    public String replaceSoldier(@RequestParam("id") int id,
+                                 @RequestParam("rank") int rank,
+                                 @RequestParam("birth") String birth,
+                                 @RequestParam("weapon") String weapon,
+                                 @RequestParam("shotpeople") int shotpeople,
+                                 Model model){
+        int statusCode = service.replaceSoldier(id, rank, birth, weapon, shotpeople);
+        model.addAttribute("statuscode", statusCode);
+        model.addAttribute("soldiers", service.getSoldiers());
+        return "soldiers";
+    }
 }
--- consumeSoldierAPI/src/main/java/hu/consumeSoldierAPI/services/SoldierService.java
+++ consumeSoldierAPI/src/main/java/hu/consumeSoldierAPI/services/SoldierService.java
@@ -1,16 +1,18 @@
 package hu.consumeSoldierAPI.services;
 
+import hu.consumeSoldierAPI.domain.Category;
 import hu.consumeSoldierAPI.domain.Soldier;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class SoldierService {
@@ -46,5 +48,28 @@ public class SoldierService {
     public void disarmSoldier(int id) {
         String url = REST_URL+"/{id}";
         restTemplate.delete(url, id);
+    }
+
+    public int updateSoldier(int id, int shot) {
+        System.out.println("update - service: "+id+", "+shot);
+        String url = REST_URL+"/{id}/{shotpeople}";
+        Map<String, Object> values = new HashMap<String, Object>();
+        values.put("id", id);
+        values.put("shotpeople", shot);
+        Set<String> keys= values.keySet();
+        for (String key : keys) {
+            System.out.println(key+" "+values.get(key));
+        }
+        // az alábbi két sor azért kell, hogy a patch-el át tudja adni az adatokat a back-end félé
+        CloseableHttpClient client = HttpClientBuilder.create().build();
+        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(client));
+
+        HttpEntity<Soldier> requestEntity = new HttpEntity<>(new Soldier());
+        ResponseEntity<Soldier> responseEntity = restTemplate.exchange(url, HttpMethod.PATCH, requestEntity, Soldier.class, values);
+        return responseEntity.getStatusCodeValue();
+    }
+
+    public int replaceSoldier(int id, int rank, String birth, String weapon, int shotpeople) {
+        return 200;
     }
 }
